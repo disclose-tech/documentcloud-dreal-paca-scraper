be9da2141d16e5bf03c6ec031a57bee3c45dd7c9
disclose-tech <152188727+disclose-tech@users.noreply.github.com>
2026-04-14 10:39:34 +0200

    Add user-agent to DocumentCloud client

--- main.py
+++ main.py
@@ -73,6 +73,11 @@ class DiscloseDREALPACAScraper(AddOn):
     def main(self):
         """Add-on main functionality."""
 
+        # User agent
+        self.client.session.headers.update(
+            {"User-Agent": "Disclose DREAL PACA Scraper Add-On"}
+        )
+
         # Add-on inputs
 
         self.run_name = self.data.get("run_name", "no name")
